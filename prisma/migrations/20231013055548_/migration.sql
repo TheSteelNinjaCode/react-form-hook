ec3175b026ad89cf373f9fdbc15d8a6ce0e2b634
Jefferson Abraham <abrahamjefferson38@gmail.com>
2023-10-12 23:58:20 -0600

    prisma migrate dev fix

--- prisma/migrations/20231013055548_/migration.sql
+++ prisma/migrations/20231013055548_/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `firstName` VARCHAR(191) NULL,
     `lastName` VARCHAR(191) NULL,
@@ -10,8 +10,8 @@ CREATE TABLE `users` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `users_login_key`(`login`),
-    UNIQUE INDEX `users_email_key`(`email`),
-    UNIQUE INDEX `users_login_email_key`(`login`, `email`),
+    UNIQUE INDEX `Users_login_key`(`login`),
+    UNIQUE INDEX `Users_email_key`(`email`),
+    UNIQUE INDEX `Users_login_email_key`(`login`, `email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
